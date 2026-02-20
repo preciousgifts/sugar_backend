f5df8bd9944441e7c88eae25f56e4ef1c15d4f86
preciousgifts <obabiyiolusayo@gmail.com>
2026-02-20 22:33:06 +0100

    fixing auth error

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/authController.js";
-import authMiddleware from "../middleware/authmiddleware.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/sign-up", registerUser);
-authRoutes.post("/login", loginUser);
+authRoutes.post("/login", loginUser, authMiddleware);
 
 export { authRoutes };
